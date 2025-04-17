8f178e6af22a860cd307680f50278a74169729dd
haodai <a596116@gmail.com>
2025-04-17 22:08:32 +0800

    重構docker-compose.yml和Dockerfile，將服務名稱更改為frontend和backend，調整目錄結構及環境變數設置，新增健康檢查和資源限制配置，以提升服務穩定性和性能。

---
+++ express-upload-service/ecosystem.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'qrcode',
+      script: './.output/server/index.mjs',
+      instances: 'max', // 負載平衡模式下的 cpu 數量
+      exec_mode: 'cluster', // cpu 負載平衡模式
+      max_memory_restart: '10G', // 緩存了多少記憶體重新整理
+      env_prod: {
+        name: 'qrcode_prod',
+        PORT: 3023, //指定伺服器上的 port
+        NODE_ENV: 'prod',
+      },
+    },
+  ],
+}
